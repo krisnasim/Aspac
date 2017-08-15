40359177f49d86549d2f5e89a534982d70aee51d
krisnasim <jonathan.simananda@outlook.com>
2017-08-15 18:22:18 +0700

    -first app initial

---
+++ app/src/main/java/id/co/ncl/aspac/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package id.co.ncl.aspac.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.ncl.aspac.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
