b776232f317a771c3fcffe2993e1a67c4a5c72a0
krisnasim <jonathan.simananda@outlook.com>
2017-08-16 19:39:42 +0700

    -added login activity, designed using constraintlayout -added home activity for main navigation -adding icons used to differentiate each menu on home activity navigation -added attendance, create, home, leave, machine, sparepart, and work fragment

--- app/src/main/java/id/co/ncl/aspac/activity/LoginActivity.java
+++ app/src/main/java/id/co/ncl/aspac/activity/LoginActivity.java
@@ -1,15 +1,26 @@
 package id.co.ncl.aspac.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import id.co.ncl.aspac.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @OnClick(R.id.loginButton)
+    public void login() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 }
