4aa8e6d0861880b58bea158529289a8c4ce8e2fd
Jonathan Simananda <jonathan.simananda@gmail.com>
2017-11-08 01:02:59 +0700

    -creating new layout for LPS form -adding new library for expandable layout -updating gradle ver from 2.3.3 to 3.0.0

--- app/src/main/java/id/co/ncl/aspac/fragment/CreateFragment.java
+++ app/src/main/java/id/co/ncl/aspac/fragment/CreateFragment.java
@@ -7,11 +7,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import id.co.ncl.aspac.R;
 
 public class CreateFragment extends Fragment {
+
+    @BindView(R.id.expandableLayout1) ExpandableRelativeLayout expandableLayout1;
+    //@BindView(R.id.expandableLayout2) ExpandableRelativeLayout expandableLayout2;
+    @BindView(R.id.expandableButton1) Button expandableButton1;
+
+    View view;
+
+    @OnClick(R.id.expandableButton1)
+    public void clickMe() {
+        expandableLayout1.toggle();
+    }
+
+//    @OnClick(R.id.expandableLayout1)
+//    public void clickme() {
+//        expandableButton1(view);
+//    }
+
+//    @OnClick(R.id.expandableLayout2)
+//    public void clickme2() {
+//        expandableButton2(view);
+//    }
 
     public CreateFragment() {
         // Required empty public constructor
@@ -26,7 +52,7 @@ public class CreateFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_create, container, false);
+        view = inflater.inflate(R.layout.fragment_create, container, false);
         ButterKnife.bind(this, view);
         getActivity().setTitle("Penulisan LPS");
 
@@ -41,5 +67,15 @@ public class CreateFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    public void expandableButton1(View view) {
+        //expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
+        //expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton2(View view) {
+        //expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
+        //expandableLayout2.toggle(); // toggle expand and collapse
     }
 }
